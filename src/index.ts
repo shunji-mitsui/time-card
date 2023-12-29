2270a00f268ab4950157e740b0fb11de666b1a7b
shunji-mitsui <shunji0624mm@gmail.com>
2023-12-30 08:28:20 +0900

    first deploy

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,33 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { hello } from './example-module';
 
-console.log(hello());
+import { register } from './register';
+import { start } from './start';
+import { stop } from './stop';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function doPost(e: GoogleAppsScript.Events.DoPost) {
+  // TODO 月ごとにシートを分ける処理を入れる
+  const ss = SpreadsheetApp.getActiveSpreadsheet(); // 現在開いているスプレッドシートを取得
+  const sheet = ss.getSheetByName('シート1'); // 書き込むシートを指定（シート名を変更してください）
+  const slackID = e.parameter.user_id;
+  const command = e.parameter.command;
+  if (!sheet) {
+    return ContentService.createTextOutput(
+      '無効なコマンドです。管理者に問い合わせてください'
+    );
+  }
+  if (command === '/start') {
+    return start(sheet, slackID);
+  } else if (command === '/stop') {
+    return stop(sheet, slackID);
+  } else if (command === '/register') {
+    return register(sheet, slackID, e.parameter.text);
+  } else {
+    // 無効なコマンド。ここに来ることはないはず。
+    return ContentService.createTextOutput(
+      '無効なコマンドです。管理者に問い合わせてください'
+    );
+  }
+}
