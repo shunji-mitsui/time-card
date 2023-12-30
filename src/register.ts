1caa5feda2f20642fc8dc2380ec6e78a14b83060
shunji-mitsui <shunji0624mm@gmail.com>
2023-12-30 15:06:57 +0900

    fix : cellRow-> currentRow

--- src/register.ts
+++ src/register.ts
@@ -27,18 +27,18 @@ export function register(slackID: string, name: string) {
     return ContentService.createTextOutput('名前が適切でありません。');
   }
 
-  let cellRow = 1;
-  while (sheet.getRange('A' + cellRow).getValue()) {
-    if (sheet.getRange('A' + cellRow).getValue() === slackID) {
-      const prevName = sheet.getRange('B' + cellRow).getValue();
-      sheet.getRange('B' + cellRow).setValue(name);
+  let currentRow = 1;
+  while (sheet.getRange('A' + currentRow).getValue()) {
+    if (sheet.getRange('A' + currentRow).getValue() === slackID) {
+      const prevName = sheet.getRange('B' + currentRow).getValue();
+      sheet.getRange('B' + currentRow).setValue(name);
       return ContentService.createTextOutput(
         '表示名を' + prevName + 'から' + name + 'に変更しました'
       );
     }
-    cellRow += 1;
+    currentRow += 1;
   }
-  sheet.getRange('A' + cellRow).setValue(slackID);
-  sheet.getRange('B' + cellRow).setValue(name);
+  sheet.getRange('A' + currentRow).setValue(slackID);
+  sheet.getRange('B' + currentRow).setValue(name);
   return ContentService.createTextOutput(name + 'として新規登録しました。');
 }
